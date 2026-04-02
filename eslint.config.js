b8d0d893407ab13fb7c22c7ca4d376b566f28f5a
Avery66 <1064614313@qq.com>
2026-04-02 20:53:36 +0800

    Fix CI: eslint.config.js + placeholder test

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**', 'opencode-plugin/node_modules/**'],
+  },
+];
